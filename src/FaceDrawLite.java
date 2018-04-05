de5ac19c5bca9adcc606f24dc00a84c82ae80b41
bgtestlewius <williampgillespie@lewisu.edu>
2018-04-04 22:34:35 -0500

    add FaceDrawLite

---
+++ src/FaceDrawLite.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author xwxg008
+ *
+ */
+public class FaceDrawLite {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
